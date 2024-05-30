a09a386b1c59c0e8c43b6b54395490160a006357
Taras Woronjanski <taras@slingshot.finance>
2024-05-30 12:34:50 +1000

    figured it out

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,6 +2,7 @@ import type { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox-viem";
 import "@nomicfoundation/hardhat-foundry";
 
+
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
 };
--- src/components/core/pool/proof.ts
+++ src/components/core/pool/proof.ts
@@ -5,13 +5,6 @@ import { Hex, hexToBigInt} from 'viem';
 
 import { stringifyBigInts } from 'maci-crypto';
 
-const snarkjs = require('snarkjs') 
-import fs from 'fs'
-
-
-const wc = require('./artifacts/witness_calculator.js')
-
-
 const maxDepth = 32;
 
 
@@ -39,70 +32,3 @@ export type ProofInputs = {
 };
 
 
-export async function generateWitness(inputs: ProofInputs, wasmPath: string): Promise<Buffer> {
-  try {
-    const buffer = fs.readFileSync(wasmPath);
-    const witnessCalculator = await wc(buffer)
-    const witness_buffer = await witnessCalculator.calculateWTNSBin(inputs, 0);
-    return witness_buffer;
-  } catch (e) {
-    console.error("Error calculating witness: ", e)
-    throw e
-  }
-}
-
-
-
-export async function fullProve(inputs: ProofInputs, wasmPath: string, pkeyPath: string): Promise<{ proof: string; publicSignals: bigint[] }> {
-  try {
-    const buffer = fs.readFileSync(wasmPath);
-    const witnessCalculator = await wc(buffer)
-    const buff = await witnessCalculator.calculateWTNSBin(inputs, 0);
-    const { proof, publicSignals } = await snarkjs.groth16.prove(pkeyPath, buff);
-    return {
-      proof:
-        '0x' +
-        hexToBigInt(proof.pi_a[0], {size: 32}).toString(16).slice(2) +
-        hexToBigInt(proof.pi_a[1], {size: 32}).toString(16).slice(2) +
-        hexToBigInt(proof.pi_b[0][1], {size: 32}).toString(16).slice(2) +
-        hexToBigInt(proof.pi_b[0][0], {size: 32}).toString(16).slice(2) +
-        hexToBigInt(proof.pi_b[1][1], {size: 32}).toString(16).slice(2) +
-        hexToBigInt(proof.pi_b[1][0], {size: 32}).toString(16).slice(2) +
-        hexToBigInt(proof.pi_c[0], {size: 32}).toString(16).slice(2) +
-        hexToBigInt(proof.pi_c[1], {size: 32}).toString(16).slice(2),
-      publicSignals,
-    }
-  } catch (e) {
-    console.error("Error calculating witness: ", e)
-    throw e
-  }
-}
-
-
-export async function genProofCallData(inputs: ProofInputs, wasmPath: string, pkeyPath: string): Promise<{ proof: string; publicSignals: bigint[] }> {
-  try {
-    const { proof, publicSignals } = await snarkjs.groth16.fullProve(
-      inputs,
-      wasmPath,
-      pkeyPath,
-      undefined
-    )
-    return {
-      proof:
-        '0x' +
-        hexToBigInt(proof.pi_a[0], {size: 32}).toString(16).slice(2) +
-        hexToBigInt(proof.pi_a[1], {size: 32}).toString(16).slice(2) +
-        hexToBigInt(proof.pi_b[0][1], {size: 32}).toString(16).slice(2) +
-        hexToBigInt(proof.pi_b[0][0], {size: 32}).toString(16).slice(2) +
-        hexToBigInt(proof.pi_b[1][1], {size: 32}).toString(16).slice(2) +
-        hexToBigInt(proof.pi_b[1][0], {size: 32}).toString(16).slice(2) +
-        hexToBigInt(proof.pi_c[0], {size: 32}).toString(16).slice(2) +
-        hexToBigInt(proof.pi_c[1], {size: 32}).toString(16).slice(2),
-      publicSignals,
-    }
-  } catch (e) {
-    console.error("Error generating proof call data: ", e)
-    throw e
-  }
- 
-}
