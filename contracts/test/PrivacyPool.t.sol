98b38cdfca610a7e2e8c113317af128e183fa776
Taras Woronjanski <taras@slingshot.finance>
2024-07-08 12:13:06 +1000

    get rid of test-data

--- contracts/test/PrivacyPool.t.sol
+++ contracts/test/PrivacyPool.t.sol
@@ -1,1 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
 
+
+import "forge-std/Test.sol";
+import "forge-std/console.sol";
+import "forge-std/StdJson.sol";
+
+
+contract TestPrivacyPool is Test {
+
+
+}
