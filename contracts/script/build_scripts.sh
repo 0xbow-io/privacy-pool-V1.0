327f73ffb9e7b483d8e408bde5b54b3e06ac981f
Taras Woronjanski <taras@slingshot.finance>
2024-07-08 12:09:24 +1000

    ...

--- contracts/script/build_scripts.sh
+++ contracts/script/build_scripts.sh
@@ -1,2 +1,7 @@
 ##!/bin/bash
+forge install OpenZeppelin/openzeppelin-contracts
+forge install privacy-scaling-explorations/zk-kit.solidity
+forge install vimwitch/poseidon-solidity
+forge remappings > remappings.txt
+
 bun build --entrypoints ./generate_single_proof.ts --outfile generate_single_proof.js --target node
